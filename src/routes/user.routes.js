bcd52c443ea6c6f5a1045f5325e88192e7de5737
Aditya Singh <singhaditya4333@gmail.com>
2025-02-24 12:47:23 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,29 @@
-import {Router} from "express"
-import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword } from "../controllers/user.controller.js";
+import { Router } from "express"
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js"
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+
 const router = Router();
+
 router.route("/register").post(
     upload.fields([
         {
-            name: "avatar",   
+            name: "avatar",
             maxCount: 1
         },
         {
@@ -14,14 +31,25 @@ router.route("/register").post(
             maxCount: 1
         }
     ]),
-    registerUser) 
+    registerUser)
 
-    router.route("/login").post(loginUser)
+router.route("/login").post(loginUser)
 
-    //secure routes - login is necessary
-    router.route("/logout").post(verifyJWT, logoutUser)
-    router.route("/refresh-token").post(refreshAccessToken) //made endpoint for refreshing access token.
+//secure routes - login is necessary
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken) //made endpoint for refreshing access token.
 //ye route "/register" path pe jate hi hame registerUser controller pe le jata hai.
 //  ab us controller me jo bhi likha hoga vo ho jaega.
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar) //post me sari details update ho jaengi isliye patch use kiya.
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage) //upload.single isliye kyuki multer ka use krke update krne ke liye file keval ek bar upload krne ki permission denge.
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) //: is important. bcz we are taking username from params(url). before that write c or channel doesn't matter.
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router
